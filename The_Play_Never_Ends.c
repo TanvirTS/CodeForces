95e06df5c146f4ad21cdda85061ec586c452c1a2
Md Tanvir Shahriar <shahriartanvir016@gmail.com>
2025-02-28 21:40:17 +0600

    CodeForces_Round1007(Div.2)

---
+++ The_Play_Never_Ends.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    while (t--){
+        long long k;
+        scanf("%lld",&k);
+    if (k%3==1)
+        printf("YES\n");
+    else 
+        printf("NO\n");
+        
+    }
+    return 0;
+}
