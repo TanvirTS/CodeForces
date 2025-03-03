a7725109664b29f80f1737017be968c0ef1e2493
Md Tanvir Shahriar <shahriartanvir016@gmail.com>
2025-03-03 19:08:05 +0600

    136A

---
+++ Presents.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int p[n],result[n];
+  for(int i=0;i<n;i++){
+      scanf("%d",&p[i]);
+      result[p[i]-1]=i+1;
+    }
+  for (int i=0;i<n;i++)
+      printf("%d ",result[i]);
+    
+    
+    return 0;
+}
